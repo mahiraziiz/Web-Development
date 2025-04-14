e4f66868a6e8ba582dd35b924f6c1e2e78f432fa
Mahir Aziz <mahiraziz007@gmail.com>
2025-04-14 20:14:21 +0530

    coversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33sdf"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN (not a number)
+// true => 1 ; false = 0
+//undefined => NaN
+// null => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "mahir"
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // 1. true 2. false 3. true
+
+//1 = > true ; 0 => false
+// "" => false
+// "mahir" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber); // string
+console.log(stringNumber); // 33
