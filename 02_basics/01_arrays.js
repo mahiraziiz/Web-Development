cbe2c0100d0522b800d40492cb21654e668da854
Mahir Aziz <mahiraziz007@gmail.com>
2025-04-22 12:46:41 +0530

    arrays in 02_basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+// array
+
+const myArr = [2, 3, 5, 6, 6, 1];
+
+const myHeros = ["shaktiman", "naagraj"];
+
+const myArr2 = new Array()
+console.log(myArr[3]);
+
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr)
