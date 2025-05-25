a5650a110020da482bafff294f2adc4db03b461d
Mahir Aziz <mahiraziz007@gmail.com>
2025-05-25 20:48:25 +0530

    with switch case

--- 07_projects/project1/chaiorcode.js
+++ 07_projects/project1/chaiorcode.js
@@ -5,21 +5,38 @@ buttons.forEach(function (button) {
   console.log(button);
   button.addEventListener("click", function (e) {
     console.log(e);
-    console.log(e.target);   
-    if(e.target.id === "white") {
-        body.style.backgroundColor = e.target.id
-    } 
-    if(e.target.id === "black") {
-        body.style.backgroundColor = e.target.id
-    } 
-    if(e.target.id === "yellow") {
-        body.style.backgroundColor = e.target.id
-    } 
-    if(e.target.id === "green") {
-        body.style.backgroundColor = e.target.id
-    } 
-    if(e.target.id === "purple") {
-        body.style.backgroundColor = e.target.id
-    } 
+    console.log(e.target);
+    // if (e.target.id === "white") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "black") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "yellow") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "green") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+
+    const color = e.target.id;
+    switch (color) {
+      case "white":
+        body.style.backgroundColor = color;
+        break;
+      case "black":
+        body.style.backgroundColor = color;
+        break;
+      case "yellow":
+        body.style.backgroundColor = color;
+        break;
+      case "green":
+        body.style.backgroundColor = color;
+        break;
+      case "purple":
+        body.style.backgroundColor = color;
+        break;
+      default:
+        console.log("Invalid color:", color);
+    }
   });
 });
