73007c94947acd2e18b74a3fd2b5a9b7dfe99fbc
Mahir Aziz <152379623+mahiraziiz@users.noreply.github.com>
2025-06-14 13:48:22 +0530

    Delete 07_projects directory

--- 07_projects/project3/chaiorcode.js
+++
@@ -1,9 +0,0 @@
-const clock = document.getElementById("clock");
-// document.querySelector('#clock');
-
-setInterval(function () {
-  const date = new Date();
-  //   console.log(date.toLocaleTimeString());
-  clock.innerHTML = date.toLocaleTimeString();
-  // clock.innerHTML = date.toLocaleDateString();
-}, 1000);
