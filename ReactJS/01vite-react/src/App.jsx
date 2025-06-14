718bb6f90e8e3d3fd1866575f220592003f72869
Mahir Aziz <mahiraziz007@gmail.com>
2025-06-14 13:29:41 +0530

    react-basics

---
+++ ReactJS/01vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <div>
+      <Chai />
+      <h1>this is practice para</h1>
+      <p>for practice purpose</p>
+    </div>
+  );
+}
+
+export default App;
