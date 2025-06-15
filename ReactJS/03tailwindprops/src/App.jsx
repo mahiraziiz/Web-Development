8036a411173b2f46cc248099a9c7e2605c76e4fa
Mahir Aziz <mahiraziz007@gmail.com>
2025-06-15 18:17:47 +0530

    Props explain

---
+++ ReactJS/03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-400 text-black p-5 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card/>
+    </>
+  ); 
+}
+
+export default App;
---
+++ ReactJS/03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <img src="64363.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Card
