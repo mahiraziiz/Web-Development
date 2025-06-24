d17a749d1c5287e9093fe6dca54593bc6b53cac6
Mahir Aziz <mahiraziz007@gmail.com>
2025-06-24 13:39:38 +0530

    react Router Project

---
+++ ReactJS/07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Finally Solved Error</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
