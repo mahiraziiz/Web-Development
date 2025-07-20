cfcbc0a879214c79fe92bbff796ddc433d52cd16
Mahir Aziz <mahiraziz007@gmail.com>
2025-07-20 17:43:58 +0530

    add video and user model

---
+++ Backend/Professional/src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-paginate-v2";
+const videoSchema = new screen(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    discription: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: String,
+      default: 0,
+    },
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
