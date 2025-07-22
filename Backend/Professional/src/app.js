34ca122e29ded7fc63e13690c3332e2933116e46
Mahir Aziz <mahiraziz007@gmail.com>
2025-07-22 20:16:16 +0530

    routes done in backend

--- Backend/Professional/src/app.js
+++ Backend/Professional/src/app.js
@@ -15,4 +15,12 @@ app.use(express.json({ limit: "15kb" }));
 app.use(express.urlencoded({ extended: true, limit: "15kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//https://localhost:8000/api/v1/users/register
 export default app;
--- Backend/Professional/src/index.js
+++ Backend/Professional/src/index.js
@@ -1,5 +1,6 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config({ path: "./env" });
 connectDB()
