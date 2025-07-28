afed4f645d7c4979e7a59f8e6e56092afbff23cc
Mahir Aziz <mahiraziz007@gmail.com>
2025-07-28 23:49:24 +0530

    user controller work

--- Backend/Professional/src/controllers/user.controller.js
+++ Backend/Professional/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -13,6 +14,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // remove password and refresh token from response
   // check for user creation
   // return response else error
+
   if (!req.body) {
     return res.status(400).json({ message: "Request body is missing" });
   }
@@ -21,14 +23,12 @@ const registerUser = asyncHandler(async (req, res) => {
   console.log("email: ", email);
 
   if (
-    [fullName, email, username, password].some((field) => {
-      field?.trim() === "";
-    })
+    [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -37,18 +37,32 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+  
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar File is required");
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
+
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  console.log("req.files:", req.files);
+  console.log("req.body:", req.body);
 
   if (!avatar) {
     throw new ApiError(400, "Avatar File is required");
   }
+
   if (!coverImage) {
     throw new ApiError(400, "Cover Image is required");
   }
@@ -62,9 +76,10 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findById(user._id).select(
+  const createdUser = await User.findById(User._id).select(
     "-password -refreshToken"
   );
+
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
--- Backend/Professional/src/models/user.model.js
+++ Backend/Professional/src/models/user.model.js
@@ -53,13 +53,14 @@ const userSchema = mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) return next;
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
 userSchema.methods.generateAccessToken = function () {
   return (
     jwt.sign(
@@ -76,6 +77,7 @@ userSchema.methods.generateAccessToken = function () {
     }
   );
 };
+
 userSchema.methods.generateRefreshToken = function () {
   return (
     jwt.sign(
@@ -89,4 +91,5 @@ userSchema.methods.generateRefreshToken = function () {
     }
   );
 };
+
 export const User = mongoose.model("User", userSchema);
